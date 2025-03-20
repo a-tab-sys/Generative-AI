739478ab424660563be17e2f176486bb0c33cd0b
a-tab-sys <ammaarah.t33@gmail.com>
2025-03-20 15:08:59 -0400

    eighth commit

--- LANGCHAIN/api's/app.py
+++ LANGCHAIN/api's/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from langchain.prompt import ChatPromptTemplae
+from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langserve import add_routes
 import uvicorn
@@ -7,11 +7,13 @@ import os
 from langchain_community.llms import Ollama
 from dotenv import load_dotenv
 
+# creating an open ai api???
+
 load_dotenv()  #helps me initialize my enviroment variables
 
 os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 
-##In order to create a fast api, we need to first creae an app
+##In order to create a fast api, we need to first create an app
 ##later we will keep adding routes to this app
 app=FastAPI(
     title="Langchain Server",
@@ -19,22 +21,25 @@ app=FastAPI(
     description="A simple API Server"
 )
 
+# one way to add route
 add_routes(
     app,
     ChatOpenAI(),   # model we will use
     path="/openai"
 )
 
+# initialize 1 model
 model=ChatOpenAI()
-#ollama llama2
+
+#call our other model ollama llama2
 llm=Ollama(model="llama2")
 
 # make 2 prompts templates
 # prompt1 will interact with chat open api
-# prompt2 will interact with llm model
+# prompt2 will interact with out open source llm model
 
 prompt1=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
-prompt2=ChatPromptTemplate.from_template("Write me an essay about {topic} with 100 words")
+prompt2=ChatPromptTemplate.from_template("Write me a poem about {topic} with 100 words")
 
 add_routes(
    app,
@@ -51,7 +56,15 @@ add_routes(
 )
 
 if __name__=="__main__":
-    # this application we can ru in any server that we want. this is the first step towards building a production grade application
+    # this application we can run in any server that we want. this is the first step towards building a production grade application
     uvicorn.run(app, host="localhost", port=8000)
+    # uvicorn.run(app, host="localhost", port=8080)  # another port option
 
 
+
+# RUN WITH
+# python app.py
+
+# whn you run this open the local host, youll get a detail not found inda message. if you want to see the entire api creted add /docs to the url. this will show  you the entire langchain server. this is called swagger ui.
+
+# NOW WE HAVE CRATED THE API'S....WE NOW NEEED SOME WAY TO INTERACT WITH THESE APIS -> client.py
--- LANGCHAIN/api's/client.py
+++ LANGCHAIN/api's/client.py
@@ -1,1 +1,37 @@
 # pip install -r requirements.txt
+
+# api part is created, now we need some way to interact with the apis
+# we are creating a web app with a front end that will interact with our api
+
+import requests
+import streamlit as st
+
+def get_openai_response(input_text):
+    #will use request obect to create a response and call the api
+    response=requests.post("http://localhost:8000/essay/invoke", json={"input": {"topic": input_text}})
+    # response = requests.post("http://localhost:8080/essay/invoke", json={"input": {"topic": input_text}}) #another port option
+    return response.json()['output']['content']
+
+def get_ollama_response(input_text):
+    response=requests.post("http://localhost:8000/poem/invoke", json={"input": {"topic": input_text}})
+    # response = requests.post("http://localhost:8080/poem/invoke", json={"input": {"topic": input_text}})  #another port option
+    return response.json()['output']
+
+##streamlit framework
+
+st.title('Langchain Demo With LLAMA2 API')
+input_text=st.text_input("Write an essay on")
+input_text1=st.text_input("Write a poem on")
+
+if input_text:
+    st.write(get_openai_response(input_text))
+
+if input_text1:
+    st.write(get_ollama_response(input_text1))
+
+# RUN WITH
+# streamlit run client.py
+
+# btw you need to have app.py running in one terminal and client.py running in another terminal.
+# they need to be running simultaneousy
+# open ai api wont work because you dont have a api key for it. 
